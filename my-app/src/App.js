2ff443b73b02929946b686a8ba61a58da6e442af
Rachel Hofer <rhofer3@gmail.com>
2019-01-06 16:54:09 -0500

    react-app created

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from  './Components/Main'
+import {Route} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Route  component= {Main} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/Components/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+
+class Main extends Component{
+
+
+    render(){
+
+        return(
+
+            <div>
+                Is this working!
+            </div>
+
+        )
+    }
+
+}
+
+export default Main;
